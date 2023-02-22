aa045e01f8bacde57cc11d5b77d082cf70fde9fc
Ryan-Jones1999 <ryan.jones@kainos.com>
2023-02-22 16:50:25 +0000

    integrated webhooks into working example

--- CheckoutExamples/bankTransfer/server.js
+++ CheckoutExamples/bankTransfer/server.js
@@ -2,7 +2,7 @@ const nunjucks = require('nunjucks');
 const express = require('express');
 const app = express();
 require('dotenv').config()
-
+const endpointSecret = process.env.ENDPOINT_SECRET
 const stripe = require("stripe")(
   process.env.API_KEY
 );
@@ -41,6 +41,7 @@ stripe test_helpers customers fund_cash_balance "<customer.id>" \
 
 */
 app.post('/create-checkout-session', async (req, res) => {
+
   const customer = await stripe.customers.create();
 
   const session = await stripe.checkout.sessions.create({
@@ -65,13 +66,49 @@ app.post('/create-checkout-session', async (req, res) => {
       quantity: 1,
     }],
     mode: 'payment',
-    success_url: 'http://localhost:3000/success',
-    cancel_url: 'http://localhost:3000/create-checkout-session',
+    success_url: 'http://localhost:4242/success',
+    cancel_url: 'http://localhost:4242/create-checkout-session',
   });
 
   res.redirect(303, session.url);
 });
 
-app.listen(3000, () => console.log(`Listening on port ${3000}!`));
+/*
+See the readme and make sure the ENDPOINT_SECRET is set - see the word document at chapter 4.2 for more information on how to get set up.
+*/
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      console.log('payment succeeded')
+      break;
+
+    case 'charge.succeeded':
+    console.log('charge succeeded')
+    break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+app.listen(4242, () => console.log(`Listening on port ${4242}!`));
 
   
